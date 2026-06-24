2f9716a998f34f4f899cd2d7e2547a31174d2937
David Li <101304012+DNKYr@users.noreply.github.com>
2026-06-24 00:12:35 -0700

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        n, m, res = len(grid), len(grid[0]), 0
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        direction = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        def is_valid_next(i, j):
+            return i >= 0 and j >= 0 and i < n and j < m and grid[i][j] == "1" and not visited[i][j]
+        def dfs(i, j):
+            visited[i][j] = True
+            for c, r in direction:
+                next_i = i + c
+                next_j = j + r
+                if is_valid_next(next_i, next_j):
+                    dfs(next_i, next_j)
+        for i in range(n):
+            for j in range(m):
+                if is_valid_next(i, j):
+                    dfs(i, j)
+                    res += 1
+        return res
+
+
+        
+        
